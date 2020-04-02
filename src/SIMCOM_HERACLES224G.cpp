e0e321e10f5096818e9538876a62ecb0c1882e20
j_delsol_catie <j.delsol@catie.fr>
2020-04-02 16:53:40 +0200

    add methods to manage power ON/OFF

--- src/SIMCOM_HERACLES224G.cpp
+++ src/SIMCOM_HERACLES224G.cpp
@@ -21,10 +21,13 @@
 #include "SIMCOM_HERACLES224G.h"
 #include "SIMCOM_HERACLES224G_CellularContext.h"
 #include "SIMCOM_HERACLES224G_CellularNetwork.h"
+#include <CellularLog.h>
 
 using namespace mbed;
 using namespace rtos;
 using namespace events;
+
+#define DEVICE_READY_URC "CPIN:"
 
 #if !defined(MBED_CONF_SIMCOM_HERACLES224G_PWR)
 #define MBED_CONF_SIMCOM_HERACLES224G_PWR    NC
@@ -65,10 +68,11 @@ static const intptr_t cellular_properties[AT_CellularDevice::PROPERTY_MAX] = {
     20,  // PROPERTY_AT_SEND_DELAY
 };
 
-SIMCOM_HERACLES224G::SIMCOM_HERACLES224G(FileHandle *fh, PinName pwr, bool active_high)
+SIMCOM_HERACLES224G::SIMCOM_HERACLES224G(FileHandle *fh, PinName pwr_key, bool active_high, PinName rst)
     : AT_CellularDevice(fh),
       _active_high(active_high),
-      _pwr_key(pwr, !_active_high)
+      _pwr_key(pwr_key, !_active_high),
+	  _rst(rst, !_active_high)
 {
     set_cellular_properties(cellular_properties);
 }
@@ -80,14 +84,6 @@ nsapi_error_t SIMCOM_HERACLES224G::init()
         return err;
     }
     _at.lock();
-
-    // AT+CMEE=2
-    // Set command disables the use of result code +CME ERROR: <err> as an indication of an
-    // error relating to the +Cxxx command issued. When enabled, device related errors cause the +CME
-    // ERROR: <err> final result code instead of the default ERROR final result code. ERROR is returned
-    // normally when the error message is related to syntax, invalid parameters or DTE functionality.
-    // Current setting: enable and use verbose <err> values
-    _at.at_cmd_discard("+CMEE", "=2");
 
 
     return _at.unlock_return_error();
@@ -108,38 +104,6 @@ CellularDevice *CellularDevice::get_default_instance()
 }
 #endif
 
-nsapi_error_t SIMCOM_HERACLES224G::hard_power_on()
-{
-    soft_power_on();
-
-    return NSAPI_ERROR_OK;
-}
-
-nsapi_error_t SIMCOM_HERACLES224G::soft_power_on()
-{
-    _pwr_key = _active_high;
-    ThisThread::sleep_for(500);
-    _pwr_key = !_active_high;
-    ThisThread::sleep_for(5000);
-    _pwr_key = _active_high;
-    ThisThread::sleep_for(5000);
-
-    return NSAPI_ERROR_OK;
-}
-
-nsapi_error_t SIMCOM_HERACLES224G::hard_power_off()
-{
-    _pwr_key = !_active_high;
-    ThisThread::sleep_for(10000);
-
-    return NSAPI_ERROR_OK;
-}
-
-nsapi_error_t SIMCOM_HERACLES224G::soft_power_off()
-{
-    return AT_CellularDevice::soft_power_off();
-}
-
 AT_CellularNetwork *SIMCOM_HERACLES224G::open_network_impl(ATHandler &at)
 {
     return new SIMCOM_HERACLES224G_CellularNetwork(at, *this);
@@ -148,4 +112,92 @@ AT_CellularNetwork *SIMCOM_HERACLES224G::open_network_impl(ATHandler &at)
 AT_CellularContext *SIMCOM_HERACLES224G::create_context_impl(ATHandler &at, const char *apn, bool cp_req, bool nonip_req)
 {
     return new SIMCOM_HERACLES224G_CellularContext(at, this, apn, cp_req, nonip_req);
+}
+
+void SIMCOM_HERACLES224G::set_ready_cb(Callback<void()> callback)
+{
+    _at.set_urc_handler(DEVICE_READY_URC, callback);
+}
+
+nsapi_error_t SIMCOM_HERACLES224G::soft_power_on()
+{
+    if (_pwr_key.is_connected()) {
+	   tr_info("SIMCOM_HERACLES224G::soft_power_on");
+	   // check if modem was powered on already
+	   if (!wake_up()) {
+		   if (!wake_up(true)) {
+			   tr_error("Modem not responding");
+			   soft_power_off();
+			   return NSAPI_ERROR_DEVICE_ERROR;
+		   }
+	   }
+   }
+
+    return NSAPI_ERROR_OK;
+}
+
+nsapi_error_t SIMCOM_HERACLES224G::soft_power_off()
+{
+    _at.lock();
+    _at.cmd_start("AT+CPOWD=1");
+    _at.cmd_stop_read_resp();
+    if (_at.get_last_error() != NSAPI_ERROR_OK) {
+        tr_warn("Force modem off");
+        if (_pwr_key.is_connected()) {
+            press_button(_pwr_key, 1500); // Heracles_Hardware_Design_V1.02: Power off signal at least 1500 ms
+        }
+    }
+    return _at.unlock_return_error();
+}
+
+void SIMCOM_HERACLES224G::press_button(DigitalOut &button, uint32_t timeout)
+{
+    if (!button.is_connected()) {
+        return;
+    }
+    button = _active_high;
+    ThisThread::sleep_for(timeout);
+    button = !_active_high;
+}
+
+bool SIMCOM_HERACLES224G::wake_up(bool reset)
+{
+    // check if modem is already ready
+    _at.lock();
+    _at.flush();
+    _at.set_at_timeout(30);
+    _at.cmd_start("AT");
+    _at.cmd_stop_read_resp();
+    nsapi_error_t err = _at.get_last_error();
+    _at.restore_at_timeout();
+    _at.unlock();
+    // modem is not responding, power it on
+    // modem is not responding, power it on
+     if (err != NSAPI_ERROR_OK) {
+         if (!reset) {
+             tr_info("Power on modem");
+             press_button(_pwr_key, 1000);
+         } else {
+             tr_warn("Reset modem");
+             // TODO: implement rst pin in the HW design
+             if (_rst.is_connected()) {
+                 press_button(_rst, 150);
+             }
+
+         }
+         _at.lock();
+         // According to Heracles_Hardware_Design_V1.02, serial_port is active after 3s, but it seems to take over 5s
+         _at.set_at_timeout(5000);
+         _at.resp_start();
+         _at.set_stop_tag("RDY");
+         bool rdy = _at.consume_to_stop_tag();
+         _at.set_stop_tag(OK);
+         _at.restore_at_timeout();
+         _at.unlock();
+         if (!rdy) {
+             return false;
+         }
+     }
+    // sync to check that AT is really responsive and to clear garbage
+    return _at.sync(500);
 }
